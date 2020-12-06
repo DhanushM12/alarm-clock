9123ace017414d59a1b0115111c4d58c7683e163
DhanushM12 <dhanushmukhi@gmail.com>
2020-12-07 04:59:36 +0530

    added the digital alarm clock functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+  setInterval(updateTime, 1000);
   return (
     <div className="App">
-      Alarm Clock
+      Digital Alarm Clock
+  <h1>{currentTime}</h1>
     </div>
   );
 }
